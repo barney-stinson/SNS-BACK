73456ddf0b9e2ac280511185e9f4a97a88b4d06d
barney-stinson <iit2018081@iiita.ac.in>
2020-05-04 01:30:32 +0530

    Update groupChatRouter.js

--- routes/groupChatRouter.js
+++ routes/groupChatRouter.js
@@ -44,6 +44,67 @@ groupChatRouter.route('/')
         err.status = 404;
         return next(err);
     }
+})
+.put(cors.corsWithOptions, authenticate.verifyUser, (req, res, next) => {
+    res.statusCode = 403;
+    res.end('PUT operation not supported on /groupChats/');
+})
+.delete(cors.corsWithOptions, authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
+    GroupChats.remove({})
+    .then((resp) => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(resp);
+    }, (err) => next(err))
+    .catch((err) => next(err));    
 });
+
+
+groupChatRouter.route('/:groupChatId')
+.options(cors.corsWithOptions, (req, res) => { res.sendStatus(200); })
+.get(cors.corsWithOptions, (req,res,next) => {
+    GroupChats.findById(req.params.groupChatId)
+    .populate('user')
+    .then((groupChat) => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(groupChat);
+    }, (err) => next(err))
+    .catch((err) => next(err));
+})
+.post(cors.corsWithOptions, authenticate.verifyUser, (req, res, next) => {
+    res.statusCode = 403;
+    res.end('POST operation not supported on /groupChats/');
+})
+.put(cors.corsWithOptions, authenticate.verifyUser, (req, res, next) => {
+    res.statusCode = 403;
+    res.end('PUT operation not supported on /groupChats/');
+})
+.delete(cors.corsWithOptions, authenticate.verifyUser, (req, res, next) => {
+    GroupChats.findById(req.params.groupChatId)
+    .then((groupChat) => {
+        if (groupChat != null) {
+            if (!groupChat.user.equals(req.user._id)) {
+                var err = new Error('You are not authorized to delete this message!');
+                err.status = 403;
+                return next(err);
+            }
+            GroupChats.findByIdAndRemove(req.params.groupChatId)
+            .then((resp) => {
+                res.statusCode = 200;
+                res.setHeader('Content-Type', 'application/json');
+                res.json(resp); 
+            }, (err) => next(err))
+            .catch((err) => next(err));
+        }
+        else {
+            err = new Error('Message ' + req.params.groupChatId + ' not found');
+            err.status = 404;
+            return next(err);            
+        }
+    }, (err) => next(err))
+    .catch((err) => next(err));
+});
+
 
 module.exports  = groupChatRouter;
