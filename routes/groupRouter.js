a327e159ef9fcebe9eeec17846dd44a94af49ddd
barney-stinson <iit2018081@iiita.ac.in>
2020-05-03 18:42:10 +0530

    groups info now can be updated and also group can be deleted by the group admin only

--- routes/groupRouter.js
+++ routes/groupRouter.js
@@ -46,6 +46,95 @@ groupRouter.route('/')
         err.status = 404;
         return next(err);
     }
+})
+.put(cors.corsWithOptions, authenticate.verifyUser, (req, res, next) => {
+    res.statusCode = 403;
+    res.end('PUT operation not supported on /groups');
+})
+.delete(cors.corsWithOptions, authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
+    Groups.remove({})
+    .then((resp) => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(resp);
+    }, (err) => next(err))
+    .catch((err) => next(err));        
+});
+
+
+groupRouter.route('/:groupId')
+.options(cors.corsWithOptions, (req, res) => {res.sendStatus(200); })
+.get(cors.corsWithOptions, authenticate.verifyUser,(req,res,next) => {
+    Groups.findById(req.params.groupId)
+    .populate('admin')
+    .populate('users')
+    .then((group) => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(group);
+    }, (err) => next(err))
+    .catch((err) => next(err));
+})
+.post(cors.corsWithOptions, (req,res,next) => {
+    req.statusCode=403;
+    res.end('POST operation not supported on /groups/');
+})
+.put(cors.corsWithOptions, authenticate.verifyUser, (req,res,next) => {
+    Groups.findById(req.params.groupId)
+    .then((group) => {
+        if (group != null) {
+            if (!group.admin.equals(req.user._id)) {
+                var err = new Error('You are not authorized to update this group info!');
+                err.status = 403;
+                return next(err);
+            }
+            req.body.admin = req.user._id;
+            Groups.findByIdAndUpdate(req.params.groupId, {
+                $set: req.body
+            }, { new: true })
+            .then((group) => {
+                Groups.findById(group._id)
+                .populate('admin')
+                .populate('users')
+                .then((group) => {
+                    res.statusCode = 200;
+                    res.setHeader('Content-Type', 'application/json');
+                    res.json(group); 
+                })               
+            }, (err) => next(err));
+        }
+        else {
+            err = new Error('Group ' + req.params.groupId + ' not found');
+            err.status = 404;
+            return next(err);            
+        }
+    }, (err) => next(err))
+    .catch((err) => next(err));
+})
+.delete(cors.corsWithOptions, authenticate.verifyUser, (req,res,next) => {
+    Groups.findById(req.params.groupId)
+    .then((group) => {
+        if (group != null) {
+            if (!group.admin.equals(req.user._id)) {
+                var err = new Error('You are not authorized to delete this notice!');
+                err.status = 403;
+                return next(err);
+            }
+            Groups.findByIdAndRemove(req.params.groupId)
+            .then((resp) => {
+                res.statusCode = 200;
+                res.setHeader('Content-Type', 'application/json');
+                res.json(resp); 
+            }, (err) => next(err))
+            .catch((err) => next(err));
+        }
+        else {
+            err = new Error('Group ' + req.params.groupId + ' not found');
+            err.status = 404;
+            return next(err);            
+        }
+    }, (err) => next(err))
+    .catch((err) => next(err));
 });
 
 
